38bf135fb71864cb1407b610a44c0297df7b401b
Frenco <hey@frenco.dev>
2022-07-27 23:23:02 +0630

    feat: update prettier config

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,10 +1,23 @@
 /** @type {import('prettier').Config} */
+const overridableDefaults = {
+  // these can be overridden by the user's editorconfig
+  endOfLine: 'lf',
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false
+}
+
+/** @type {import('prettier').Config} */
 module.exports = {
-  bracketSpacing: false,
-  jsxSingleQuote: true,
+  ...overridableDefaults,
+  // save space
   semi: false,
+  bracketSpacing: false,
+  // use single quotes everywhere
   singleQuote: true,
-  trailingComma: 'none',
+  jsxSingleQuote: true,
+  // for better git diffs
+  trailingComma: 'all',
   plugins: [
     require.resolve('prettier-plugin-packagejson'),
     require.resolve('prettier-plugin-organize-imports')
